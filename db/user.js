a6919c1364df2ff3f90ee7ea11a513c84a59b231
anasdove2020 <anasdove2020@gmail.com>
2021-06-26 23:44:06 +0700

    Update Modiv Properties in Get And GetDetail

--- db/user.js
+++ db/user.js
@@ -1,22 +1,35 @@
 const db = require('./index');
 
 const getUsers = async () => {
-    const {rows} = await db.query(`SELECT 
-                        id, email, first_name, last_name, phone, dob, marital_status, occupation, broker_dealer_association,
-                        has_investor_license, secondary_email, backup_with_holding 
-                    FROM "Users" 
-                    ORDER BY Id DESC
-                    LIMIT 10`, null);
+    const {rows} = await db.query(`SELECT US.id AS "UserId", US.email AS "Email", US.first_name AS "FirstName", US.last_name AS "LastName",
+                                        US.phone AS "Phone", US.dob AS "DateOfBirth", US.marital_status AS "MaritalStatus", US.occupation AS "Occupation",
+                                        US.broker_dealer_association AS "BrokerDealerAssociation", US.has_investor_license AS "HasInvestorLicense",
+                                        US.secondary_email AS "SecondaryEmail", US.backup_with_holding AS "BackupWithHolding", US.citizenship_id AS "CitizenshipId", CTZ.type AS "CitizenshipType",
+                                        US.address_id AS "AddressId", RESADD.street AS "ResidentAddressStreet", RESADD.city AS "ResidentAddressCity"
+                                    FROM
+                                        "Users" US
+                                        LEFT JOIN "Citizenships" CTZ
+                                            ON US.citizenship_id = CTZ.id
+                                        LEFT JOIN "Addresses" RESADD
+                                            ON US.address_id = RESADD.id
+                                    ORDER BY US.Id DESC LIMIT 10;`, null);
     
     return rows;
 };
 
 const getUserById = async(id) => {
-    const {rows} = await db.query(`SELECT 
-                        id, email, first_name, last_name, phone, dob, marital_status, occupation, broker_dealer_association,
-                        has_investor_license, secondary_email, backup_with_holding 
-                    FROM "Users" 
-                    WHERE id = $1`, [id]);
+    const {rows} = await db.query(`SELECT US.id AS "UserId", US.email AS "Email", US.first_name AS "FirstName", US.last_name AS "LastName",
+                                        US.phone AS "Phone", US.dob AS "DateOfBirth", US.marital_status AS "MaritalStatus", US.occupation AS "Occupation",
+                                        US.broker_dealer_association AS "BrokerDealerAssociation", US.has_investor_license AS "HasInvestorLicense",
+                                        US.secondary_email AS "SecondaryEmail", US.backup_with_holding AS "BackupWithHolding", US.citizenship_id AS "CitizenshipId", CTZ.type AS "CitizenshipType",
+                                        US.address_id AS "AddressId", RESADD.street AS "ResidentAddressStreet", RESADD.city AS "ResidentAddressCity"
+                                    FROM
+                                        "Users" US
+                                        LEFT JOIN "Citizenships" CTZ
+                                            ON US.citizenship_id = CTZ.id
+                                        LEFT JOIN "Addresses" RESADD
+                                            ON US.address_id = RESADD.id
+                                    WHERE US.id = $1`, [id]);
     
     return rows[0];
 }
